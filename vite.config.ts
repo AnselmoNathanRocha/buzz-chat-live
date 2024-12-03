5c2bbd267c8e5b5d702673eeb3d9ed2d160397fb
Anselmo Nathan Rocha <anselmonathanrocha16@gmail.com>
2024-12-03 03:46:06 -0300

    Correção

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,64 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*$/,
+            handler: "CacheFirst", 
+            options: {
+              cacheName: "google-fonts",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/.*\.(png|jpg|jpeg|gif|svg)$/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 20,
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "Buzz Chat Live",
+        short_name: "BuzzChat",
+        description: "",
+        theme_color: "#aadddd",
+        background_color: "#aadddd",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/chat192.png", // Caminho do ícone
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/chat512.png", // Caminho do ícone
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,10 +69,9 @@ export default defineConfig({
     open: true,
   },
   optimizeDeps: {
-    include: ['stompjs'],
+    include: ["stompjs"],
   },
   define: {
     global: {},
   },
 });
-
